b172bffe7160ae121c99e69a0c64ca7afdff2b6d
Abdalla Haider Abbas <abendos203@gmail.com>
2025-02-16 23:33:42 -0800

    added reset

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 
@@ -6,12 +7,47 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private float score = 0;
     [SerializeField] private TextMeshProUGUI scoreText;
-    private FallTrigger[] pins;
+    private FallTrigger[] fallTriggers;
+    private GameObject Pins;
+    [SerializeField] private BallController ball;
+
+    [SerializeField] private GameObject pinCollection;
+
+    [SerializeField] private Transform pinAnchor;
+
+    [SerializeField] private InputManager inputManager;
     private void Start()
     {
-        pins = FindObjectsByType<FallTrigger>((FindObjectsSortMode)FindObjectsInactive.Include);
+        fallTriggers = FindObjectsByType<FallTrigger>((FindObjectsSortMode)FindObjectsInactive.Include);
 
-        foreach (FallTrigger pin in pins)
+        foreach (FallTrigger pin in fallTriggers)
+        {
+            pin.OnPinFall.AddListener(IncrementScore);
+        }
+        inputManager.OnResetPressed.AddListener(HandleReset);
+        SetPins();
+
+    }
+    private void HandleReset()
+    {
+        ball.ResetBall();
+        SetPins();
+    }
+    private void SetPins()
+    {
+        if (Pins)
+        {
+            foreach (Transform child in Pins.transform)
+            {
+                Destroy(child.gameObject);
+            }
+            Destroy(Pins);
+        }
+
+        Pins = Instantiate(pinCollection, pinAnchor.transform.position, Quaternion.identity, transform);
+
+        fallTriggers = FindObjectsByType<FallTrigger>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        foreach (FallTrigger pin in fallTriggers)
         {
             pin.OnPinFall.AddListener(IncrementScore);
         }
