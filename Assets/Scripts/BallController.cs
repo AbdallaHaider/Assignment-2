6f2ea22899ae041e7885e8d2bd1218ac3c14cf42
Abdalla Haider Abbas <abendos203@gmail.com>
2025-02-16 18:20:51 -0800

    Added ball anchor

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -2,22 +2,27 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    private bool isBallLaunched;
     [SerializeField] private float force = 1f;
+    [SerializeField] private Transform ballAnchor;
+
+    private bool isBallLaunched;
     [SerializeField] private InputManager inputManager;
-
-
     private Rigidbody ballRB;
 
     void Start()
     {
         ballRB = GetComponent<Rigidbody>();
         inputManager.OnSpacePressed.AddListener(LaunchBall);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
     }
     private void LaunchBall()
     {
         if (isBallLaunched) return;
         isBallLaunched = true;
+        transform.parent = null;
+        ballRB.isKinematic = false;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
